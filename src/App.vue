ccf5b4cad356a52c04ea2b0c2b41302d36322ebb
melodykxq <2488197963@qq.com>
2023-02-17 11:38:39 +0800

    add antd lib

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+    <a-button type="primary"/>
     <router-view/>
   </div>
 </template>
